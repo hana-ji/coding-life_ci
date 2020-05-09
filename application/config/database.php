d4ced3c45546985a1dbdf579cd4bf0937efbf35d
hana-ji <55686380+hana-ji@users.noreply.github.com>
2020-05-09 11:52:57 +0900

    템플릿으로 분리 전

--- application/config/database.php
+++ application/config/database.php
@@ -75,10 +75,10 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => '',
-	'username' => '',
-	'password' => '',
-	'database' => '', 
+	'hostname' => 'localhost',
+	'username' => 'root',
+	'password' => '123456',
+	'database' => 'opentutorials', 
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
