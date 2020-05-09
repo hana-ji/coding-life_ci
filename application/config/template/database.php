49eb4bddf66fc90f63dde49a67816b645af1cbeb
hana-ji <55686380+hana-ji@users.noreply.github.com>
2020-05-09 14:45:25 +0900

    config.php, database.php 버전 관리에서 제외

--- application/config/template/database.php
+++ application/config/template/database.php
@@ -75,10 +75,10 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
-	'username' => 'root',
-	'password' => '123456',
-	'database' => 'opentutorials', 
+	'hostname' => '',
+	'username' => '',
+	'password' => '',
+	'database' => '', 
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
